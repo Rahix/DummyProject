7949e01f5d43784de8af3d51c914e50b8811db0f
octycs <octycs@users.noreply.github.com>
2015-03-15 18:20:29 +0100

    Add mobile detection util

--- client/js/util.js
+++ client/js/util.js
@@ -4,3 +4,8 @@
 function elementID(id) {
   return document.getElementById(id);
 }
+
+// Detect mobile browser. This should work for the most browsers.
+function isMobile() {
+  return /mobi/i.test(navigator.userAgent);
+}
