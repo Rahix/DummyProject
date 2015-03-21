c9df6591b5d36fc823ee0664d8e00e6341eb62fd
Rahix <h.w.seiler@gmx.de>
2015-03-21 21:34:04 +0100

    Added some code

--- client/js/game/main.js
+++ client/js/game/main.js
@@ -1,3 +1,15 @@
 // This get automatically called
+var map_storage;
+var render_engine;
+var canvas;
+
 function main() {
+    map_storage = MapStorage();
+    render_engine = RenderEngine();
+    canvas = elementID("main-canvas");
+    setInterval(game_loop, 1/30);
+}
+
+function game_loop() {
+    render_engine.render(1, [8, 8], map_storage);
 }
---
+++ client/js/game/worldutil.js
@@ -0,0 +1,17 @@
+//This file contains a lot of functions for calculationg to and from isometric directions
+
+var iso_util = {
+    tile_scale: 256,
+    to_isometric_img_x: function(x, y, zoom) {
+        return ( (x - y) * (tile_scale / 2) - tile_scale ) * zoom;
+    },
+    to_isometric_img_y: function(x, y, zoom) {
+        return ( (x + y) * (tile_scale / 4) - (tile_scale / 2 ) ) * zoom;
+    },
+    to_isometric_x: function(x, y, zoom) {
+        return ( (x - y) * (tile_scale / 2) ) * zoom;
+    },
+    to_isometric_y: function(x, y, zoom) {
+        return ( (x + y) * (tile_scale / 4) ) * zoom;
+    }
+}
