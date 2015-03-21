fbf070a6084b9fcf9fffffe2bf61446939c02c9f
Rahix <h.w.seiler@gmx.de>
2015-03-21 21:34:04 +0100

    Added some code

--- client/js/game/render.js
+++ client/js/game/render.js
@@ -7,6 +7,12 @@ function RenderEngine(canvas)
 
     this.render = function(zoom, focus_coords, map_storage)
     {
+        render_chunk(map_storage.getChunk(0, 0), 0, 0);
+    }
+
+    // Render a chunk (16, 16)
+    this.render_chunk = function(chunk, x, y)
+    {
         
     }
 }
