fca69b7a56b3a11d7c262e8724f663c6763b4c94
Rahix <h.w.seiler@gmx.de>
2015-03-21 20:54:34 +0100

    Added some stuff

---
+++ client/js/game/render.js
@@ -0,0 +1,12 @@
+// This file contains the render engine
+
+function RenderEngine(canvas)
+{
+    this.canvas = canvas;
+    this.cctx = canvas.getContext("2d");
+
+    this.render = function(zoom, focus_coords, map_storage)
+    {
+        
+    }
+}
